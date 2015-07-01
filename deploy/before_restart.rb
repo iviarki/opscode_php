d53a655473dc39df7f02097e1742b224edcb75b6
Mark <iviarki@gmail.com>
2015-07-01 09:52:25 +0200

    More dynamic

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,11 +1,13 @@
-execute "chown" do
-  command "chown -R deploy:deploy #{node[:deploy][application][:deploy_to]}/current/config"
-  user "root"
-  action :run
-end
+node[:deploy].each do |application, deploy|
+	execute "chown" do
+	  command "chown -R deploy:deploy #{node[:deploy][application][:deploy_to]}/current/config"
+	  user "root"
+	  action :run
+	end
 
-execute "chmod" do
-  command "chmod -R 777 #{node[:deploy][application][:deploy_to]}/current/config"
-  user "root"
-  action :run
+	execute "chmod" do
+	  command "chmod -R 777 #{node[:deploy][application][:deploy_to]}/current/config"
+	  user "root"
+	  action :run
+	end
 end
